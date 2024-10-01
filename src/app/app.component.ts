1d919709d7f905244373945599a1ce9d170d828e
olmansansoefe <olman.personal@gmail.com>
2024-09-30 20:52:33 -0600

    ejercicio básico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Operator {
+  name?: string
+  symbol?: string
+}
+
+interface Calculator {
+  name?: string
+  type?: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'demo-angular-front';
+  title: string = 'demo-angular-front';
+  cant: number = 0;
+  operators: Operator[] = [
+    {
+      name: 'addition',
+      symbol: '+'
+    },
+    {
+      name: 'subtraction',
+      symbol: '-'
+    },
+  ];
+  cal: Calculator = {
+    name: 'My caculator',
+    type: 'simple'
+  };
+  date: Date = new Date(); 
+
+  operation(name: string) {
+    if (name == 'addition') {
+      this.cant++;
+    } else  if (name == 'subtraction') {
+      this.cant--;
+    }
+  }
+
 }
