1f5f8812cbf1292a6e8ea731824e1649b17ac7a1
olmansansoefe <olman.personal@gmail.com>
2025-02-17 19:57:34 -0600

    preferenceList component

---
+++ src/app/pages/preferenceList/preference-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: 'app-preference-list',
+  templateUrl: './preference-list.component.html',
+  styleUrls: ['./preference-list.component.css']
+})
+export class PreferenceListComponent {
+
+}
